badc49eaa89d763129078af46aeea8f3662ff3b4
NavanethNivol <navaneethsai1999@gmail.com>
2019-02-16 15:49:43 +0530

    git commit -m "home app is removed from settings" git status

--- profile_project/settings.py
+++ profile_project/settings.py
@@ -33,7 +33,6 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'rest_framework',
     'form.apps.FormConfig',
-    'home.apps.HomeConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
